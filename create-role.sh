a8d814562686a3f941bf3bc22161204dc111be72
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2024-02-22 14:52:22 -0500

    Create create-role.sh

---
+++ create-role.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+ACCOUNT_ID="106878672844"
+
+# Create an IAM policy document with limited permissions
+POLICY_DOCUMENT=$(cat <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "eks:DeleteCluster",
+                "eks:UpdateCluster",
+                "iam:CreateRolePolicy",
+                "iam:GetRole",
+                "iam:PassRole",
+                "iam:AttachRolePolicy",
+                "iam:DetachRolePolicy",
+                "logs:CreateLogGroup",
+                "logs:CreateLogGroup",
+                "s3:GetObject",
+                "cloudformation:*"
+            ],
+            "Resource": "*"
+        }
+      ] 
+}
+EOF
+)
+
+# Create the IAM policy
+aws iam create-policy --policy-name deployment-server-policy --policy-document "$POLICY_DOCUMENT"
+
+# Create the IAM role
+aws iam create-role --role-name deployment-server-role --assume-role-policy-document '{
+    "Version": "2012-10-17",
+    "Statement": {
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+    }
+}'
+
+# Attach the policy to the role
+aws iam attach-role-policy --policy-arn "arn:aws:iam::$ACCOUNT_ID:policy/deployment-server-policy" --role-name deployment-server-role
+
+echo "Deployment server role created successfully."
