8b993ac4ab8a21532dd5ba829b952fa1ddf02b5f
obynodavid12 <69556856+obynodavid12@users.noreply.github.com>
2024-03-04 12:06:58 -0500

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,6 +1,21 @@
-data=`yq -r '.mysqldatabase.value'  joindemo.yaml | cut -d ' ' -f 2`;
-reduced_string=$(cut -c-5 <<< "$data")
-id=`yq -e '.mysqlport.value' joindemo.yaml`
-reduced_string=$reduced_string-$id
+#!/bin/bash
 
-echo "The reduced_string: $reduced_string"
+# Set your AWS region
+export AWS_DEFAULT_REGION="us-east-2"
+
+# Define your tag key and value
+tag_key="Name"
+tag_value="devops_cloud"
+
+# Filter EBS volumes by tag and state
+volume_ids=$(aws ec2 describe-volumes --filters Name=tag:${tag_key},Values=${tag_value} Name=status,Values=available --query "Volumes[*].VolumeId" --output text)
+
+# Print the volume IDs
+echo "EBS volumes with tag ${tag_key}:${tag_value} in available state:"
+echo "$volume_ids"
+
+# Delete the volumes
+for volume_id in $volume_ids; do
+    echo "Deleting volume $volume_id"
+    aws ec2 delete-volume --volume-id $volume_id
+done
